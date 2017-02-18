d12aea74ace638111bef286a5064c628da6028a6
ice <smalltides@gmail.com>
2017-02-18 18:30:11 +0800

    add map layer control panel

--- script1.py
+++ script1.py
@@ -17,13 +17,19 @@ def color(elev):
         col = 'red'
     return col
 
+fg = folium.FeatureGroup(name = 'Volcano Locations')
+
 for lat, lon, name, elev in zip(df['LAT'], df['LON'], df['NAME'], df['ELEV']):
-    map.add_child(folium.Marker(location = [lat, lon], popup = name, icon=folium.Icon(color(elev))))
+    fg.add_child(folium.Marker(location = [lat, lon], popup = name, icon=folium.Icon(color(elev))))
+
+map.add_child(fg)
 
 map.add_child(folium.GeoJson(
     data = open('world.json'),
     name = 'World population',
     style_function = lambda x: { 'fillColor': 'green' if int(x['properties']['POP2005']) <= 10000000 else 'orange' if 10000000 < int(x['properties']['POP2005']) < 20000000 else 'red' }
 ))
+
+map.add_child(folium.LayerControl())
 
 map.save(outfile = 'test.html')
