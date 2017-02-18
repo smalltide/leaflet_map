65a05125239b43e18a9563fe78b468a208157c97
ice <smalltides@gmail.com>
2017-02-18 18:23:34 +0800

    use geojosn for render map

--- script1.py
+++ script1.py
@@ -3,7 +3,7 @@ import pandas
 
 df = pandas.read_csv('Volcanoes-USA.txt')
 
-map = folium.Map(location = [df['LAT'].mean(), df['LON'].mean()], zoom_start = 4, tiles = 'stamen Terrain' )
+map = folium.Map(location = [df['LAT'].mean(), df['LON'].mean()], zoom_start = 4, tiles = 'Mapbox Bright' )
 
 def color(elev):
     minimum = int(df['ELEV'].min())
@@ -19,5 +19,11 @@ def color(elev):
 
 for lat, lon, name, elev in zip(df['LAT'], df['LON'], df['NAME'], df['ELEV']):
     map.add_child(folium.Marker(location = [lat, lon], popup = name, icon=folium.Icon(color(elev))))
+
+map.add_child(folium.GeoJson(
+    data = open('world.json'),
+    name = 'World population',
+    style_function = lambda x: { 'fillColor': 'green' if int(x['properties']['POP2005']) <= 10000000 else 'orange' if 10000000 < int(x['properties']['POP2005']) < 20000000 else 'red' }
+))
 
 map.save(outfile = 'test.html')
