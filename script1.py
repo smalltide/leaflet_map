d1c43517b5a3a432ef9645d8f5e3388e9629adb1
ice <smalltides@gmail.com>
2017-02-18 16:53:56 +0800

    styling map marker

--- script1.py
+++ script1.py
@@ -5,7 +5,16 @@ df = pandas.read_csv('Volcanoes-USA.txt')
 
 map = folium.Map(location = [45.372, -121.697], zoom_start = 4, tiles = 'stamen Terrain' )
 
-for lat, lon, name in zip(df['LAT'], df['LON'], df['NAME']):
-    map.simple_marker(location = [lat, lon], popup = name, marker_color= 'red')
+def color(elev):
+    if elev in range(0, 1000):
+        col = 'green'
+    elif elev in range(1000, 3000):
+        col = 'orange'
+    else:
+        col = 'red'
+    return col
+
+for lat, lon, name, elev in zip(df['LAT'], df['LON'], df['NAME'], df['ELEV']):
+    map.simple_marker(location = [lat, lon], popup = name, marker_color=color(elev))
 
 map.create_map(path = 'test.html')
