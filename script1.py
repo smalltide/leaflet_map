7690cb3867df467a06a65a285fe7d4bc20609979
ice <smalltides@gmail.com>
2017-02-18 17:31:13 +0800

    revise folium to new version api

--- script1.py
+++ script1.py
@@ -18,6 +18,6 @@ def color(elev):
     return col
 
 for lat, lon, name, elev in zip(df['LAT'], df['LON'], df['NAME'], df['ELEV']):
-    map.simple_marker(location = [lat, lon], popup = name, marker_color=color(elev))
+    map.add_child(folium.Marker(location = [lat, lon], popup = name, icon=folium.Icon(color(elev))))
 
-map.create_map(path = 'test.html')
+map.save(outfile = 'test.html')
